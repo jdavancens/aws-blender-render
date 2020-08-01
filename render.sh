c5f4c911506c3103d4858964f7ef348706027a6e
Joseph Davancens <joseph.davancens@marketingevolution.com>
2020-08-01 08:40:21 -0700

    render script

--- render.sh
+++ render.sh
@@ -1,1 +1,18 @@
-blender -b home/ubuntu/blends/ocean_baked.blend -P script.py -a
+#!/bin/bash
+file_name=$1
+start_frame=$2
+end_frame=$3
+output_folder=$4
+echo Filename: $file_name
+echo Start frame: $start_frame
+echo End frame: $end_frame
+echo Output name: $output_folder
+blender \
+	--background $file_name\
+	--engine CYCLES\
+	--python "enable_gpu.py"\
+	-s $start_frame\
+	-e $end_frame\
+	--render-output "$output_folder/frame_####.png"
+
+
